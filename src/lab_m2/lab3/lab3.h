cb5783daec96e906e4956d0aff25c79635daa5c6
maritim <cristi.lambru16@gmail.com>
2025-09-29 19:12:21 +0300

    Change the order of labs in the PGAPI course

--- src/lab_m2/lab3/lab3.h
+++ src/lab_m2/lab3/lab3.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "components/simple_scene.h"
-#include "components/transform.h"
 
 
 namespace m2
@@ -15,17 +14,9 @@ namespace m2
         void Init() override;
 
      private:
-        void CreateFramebuffer(int width, int height);
-
         void FrameStart() override;
         void Update(float deltaTimeSeconds) override;
         void FrameEnd() override;
-
-        void DrawScene(Shader *shader);
-        void DrawFramebufferTextures();
-
-        void RenderSimpleMesh(Mesh *mesh, Shader *shader, const glm::mat4 &modelMatrix, Texture2D *texture1);
-        void RenderTextureScreen(Shader* shader, unsigned int textureID);
 
         void OnInputUpdate(float deltaTime, int mods) override;
         void OnKeyPress(int key, int mods) override;
@@ -36,23 +27,10 @@ namespace m2
         void OnMouseScroll(int mouseX, int mouseY, int offsetX, int offsetY) override;
         void OnWindowResize(int width, int height) override;
 
-        void LoadShader(const std::string& name);
-
      private:
-        float angle;
+        unsigned int nrInstances;
+        unsigned int maxInstances;
+        float shrink;
 
-        glm::vec3 light_position;
-        glm::vec3 light_direction;
-
-        glm::mat4 light_space_view;
-        glm::mat4 light_space_projection;
-        float light_space_near_plane;
-        float light_space_far_plane;
-
-        bool draw_framebuffer_textures;
-
-        unsigned int framebuffer_object;
-        unsigned int color_texture;
-        unsigned int depth_texture;
     };
 }   // namespace m2
